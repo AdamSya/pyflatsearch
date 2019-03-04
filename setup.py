b6f8fc07dfa08671243d828995de9fd8f5a2ad42
Adam S <adam.syanda@gmail.com>
2019-03-04 15:49:49 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyflatsearch',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/AdamSya/pyflatsearch',
+    license='MIT',
+    author='adamsya',
+    author_email='adam.syanda@gmail.com',
+    description='A data mining and visualisation tool for obtaining current information on property prices in different wards of London.'
+)
